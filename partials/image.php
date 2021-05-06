95d615f2f1b1f4ef08c642015000ef7ec95cf3da
Johnnie Munger <johnnie@wonderful.io>
2021-05-06 13:15:52 -0700

    lints

--- partials/image.php
+++ partials/image.php
@@ -5,43 +5,43 @@
  * This template should be used in 1 of 2 ways. See below;
  *
  * OPTION 1 (manual entry). Pass all attributes required to generate the image, like so:
- * 
- * 	wonder_include_template_file(
- * 		'partials/image.php',
- * 		array(
- * 			'alt' => <string>,
- * 			'attributes' => <associative_array>,
- * 			'size' => <string>,
- * 			'src' => <string>,
- * 			'srcset' => <associative_array>,
- * 		)
- * 	);
+ *
+ *  wonder_include_template_file(
+ *      'partials/image.php',
+ *      array(
+ *          'alt' => <string>,
+ *          'attributes' => <associative_array>,
+ *          'size' => <string>,
+ *          'src' => <string>,
+ *          'srcset' => <associative_array>,
+ *      )
+ *  );
  *
  * TEMPLATE USAGE:
  *
  * OPTION 2 (ACF only): If the template is utilizing ACF, you may simply pass in an
  * ACF image field, and the template will apply sane defaults:
- * 
- * 	wonder_include_template_file(
- * 		'partials/image.php',
- * 		array(
- * 			'acf' => <acf_field>,
- * 		)
- * 	);
+ *
+ *  wonder_include_template_file(
+ *      'partials/image.php',
+ *      array(
+ *          'acf' => <acf_field>,
+ *      )
+ *  );
  *
  *
  * OPTION 3 (ACF + manual entry): You may also want to combine the attributes
- * that are provided by ACF with manually enterred attributes. For example, 
+ * that are provided by ACF with manually enterred attributes. For example,
  * the below snippet would override the alt text provided by ACF:
  *
- * 	wonder_include_template_file(
- * 		'partials/image.php',
- * 		array(
- * 			'acf' => <acf_field>,
- *			'alt' => 'Use this string as the alt text, instead of the text provided by ACF'
- * 		)
- * 	);
- * 
+ *  wonder_include_template_file(
+ *      'partials/image.php',
+ *      array(
+ *          'acf' => <acf_field>,
+ *          'alt' => 'Use this string as the alt text, instead of the text provided by ACF'
+ *      )
+ *  );
+ *
  * @package Wonderpress Theme
  **/
 
@@ -53,9 +53,9 @@ $size = ( isset( $size ) ) ? $size : 'large';
 
 // Check for and generate a srcset
 // If we can't find one, we will use $src
-if($acf && isset( $acf['sizes']) ) {
+if ( $acf && isset( $acf['sizes'] ) ) {
 
-	$srcset = [];
+	$srcset = array();
 
 	$acf_src_sizes = array(
 		'medium' => array(
@@ -76,7 +76,7 @@ if($acf && isset( $acf['sizes']) ) {
 		}
 	}
 
-// ACF isn't being used. Let's look to see if $srcset was passed in
+	// ACF isn't being used. Let's look to see if $srcset was passed in
 } else {
 	$srcset = ( isset( $srcset ) ) ? $srcset : array();
 }
@@ -110,9 +110,10 @@ if ( $srcset ) {
 		<?php } ?>
 		/>
 </picture>
-<?php 
-// If we only have a single src, use a traditional <img> element.
-} else { ?>
+	<?php
+	// If we only have a single src, use a traditional <img> element.
+} else {
+	?>
 <img src="<?php echo esc_url( $src ); ?>"
 	class="<?php echo esc_attr( ( isset( $class ) ) ? $class : '' ); ?>"
 	alt="<?php echo esc_attr( $alt ); ?>" loading="lazy"
