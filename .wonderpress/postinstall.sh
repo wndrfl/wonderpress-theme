1bd2d2e0e50ea0ee2f59a7a94cfe95c962210a03
Johnnie Munger <johnnie@wonderful.io>
2021-05-06 14:14:05 -0700

    separates scripts into their own files

---
+++ .wonderpress/postinstall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cp package.json .wonderpress/archive/package.json
+cp .gitignore .wonderpress/archive/.gitignore
+cp .wonderpress/postinstall/package.json package.json
+npx statickit .
+npm uninstall -D @wndrfl/static-kit-cli
+npm prune
+cp .wonderpress/archive/.gitignore .gitignore
---
+++ .wonderpress/reset.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm -rf css gulpfile.js images js node_modules package-lock.json statickit.json
+cp .wonderpress/archive/package.json package.json
