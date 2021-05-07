8dac4c39a2afa6bc440bb0f71631eefeccb650ed
Johnnie Munger <johnnie@wonderful.io>
2021-05-06 17:15:10 -0700

    removes vendor directory upon reset

--- .wonderpress/reset.sh
+++ .wonderpress/reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -rf css gulpfile.js images js node_modules package-lock.json statickit.json
+rm -rf css gulpfile.js images js node_modules package-lock.json statickit.json vendor
 cp .wonderpress/archive/package.json package.json
