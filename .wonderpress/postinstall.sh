54783b4a4b9748dd112741364d1a3d0cdc2df1a6
Johnnie Munger <johnnie@wonderful.io>
2021-05-10 12:15:36 -0700

    adjusts npm install to not need a separate package.json

--- .wonderpress/postinstall.sh
+++ .wonderpress/postinstall.sh
@@ -2,7 +2,7 @@
 mkdir -p .wonderpress/archive
 cp package.json .wonderpress/archive/package.json
 cp .gitignore .wonderpress/archive/.gitignore
-cp .wonderpress/postinstall/package.json package.json
+# cp .wonderpress/postinstall/package.json package.json
 npx statickit .
 npm uninstall -D @wndrfl/static-kit-cli
 npm prune
