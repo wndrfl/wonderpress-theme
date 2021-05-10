e7a81cfb8b686d1b4f505ccb673d24351b7b0c24
Johnnie Munger <johnnie@wonderful.io>
2021-05-10 12:24:39 -0700

    updates scripts and improves documentation

--- .wonderpress/postinstall.sh
+++ .wonderpress/postinstall.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
+
+# Post-Install scripts
+# This script should be run after npm has completed its
+# initial installation, during the `postinstall` hook.
+
+# Create an archive directory for storing various things
 mkdir -p .wonderpress/archive
+
+# Copy the original package.json, in case we need to reset later
 cp package.json .wonderpress/archive/package.json
+
+# Copy the original .gitignore because it may be overwritten
+# during install
 cp .gitignore .wonderpress/archive/.gitignore
-# cp .wonderpress/postinstall/package.json package.json
+
+# Install Static Kit
 npx statickit .
+
+# We no longer need the Static Kit CLI, remove it
 npm uninstall -D @wndrfl/static-kit-cli
 npm prune
+
+# Copy the original .gitignore back
 cp .wonderpress/archive/.gitignore .gitignore
--- .wonderpress/reset.sh
+++ .wonderpress/reset.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
+
+# Reset scripts
+# This script should be run when attempting to reset Wonderpress
+# to it's "pre-installed" state.
+
+# Note: This script does NOT touch any PHP files. It will only reset
+# static assets and development tools.
+
+# Remove files and directories that were created during npm install
 rm -rf css gulpfile.js images js node_modules package-lock.json statickit.json vendor
+
+# Copy the original package.json from the archive, and replace
+# the modified version
 cp .wonderpress/archive/package.json package.json
